4c919367ea8b4a4845dee7a55f7497be3a04e561
Lars <lars.stetter@gmail.com>
2019-03-25 13:22:48 +0100

    java variables 8/10

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,2 +1,5 @@
-let favoriteAnimal = "cat";
-console.log("My favorite animal:"+favoriteAnimal);
+let myName = 'Lars';
+let myCity = 'Innsbruck';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
